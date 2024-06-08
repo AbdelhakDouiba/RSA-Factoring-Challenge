09daf79c23b3d16dd49831ac71357ecfcdc3143e
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-08 11:15:41 -0700

    Bad executing time

--- factors.c
+++ factors.c
@@ -1,14 +1,13 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <elf.h>
+#include <gmp.h>
 
-void calc(unsigned long long int n);
+void calc(mpz_t n);
 
 /**
 *main - calclate the factorization
@@ -21,6 +20,7 @@ int main(int ac, char **av)
 {
 	int fd_o, fd_r = 1, BUFFERSIZE = 1024;
 	char *buff, *number;
+	mpz_t num;
 
 	fd_o = open(av[1], O_RDONLY);
 	if (fd_o == -1)
@@ -49,8 +49,11 @@ int main(int ac, char **av)
 	number = strtok(buff, "\n");
 	while (number != NULL)
 	{
-		calc(strtoll(number, NULL, 10));
+		mpz_init(num);
+		mpz_set_str(num, (const char *)number, 10);
+		calc(num);
 		number = strtok(NULL, "\n");
+		mpz_clear(num);
 	}
 	free(buff);
 	close(fd_o);
@@ -61,11 +64,33 @@ int main(int ac, char **av)
 *calc - calculate and print the factor for the number
 *@n: the number
 */
-void calc(unsigned long long int n)
+void calc(mpz_t n)
 {
-	unsigned long long int a = 2;
+	int flag = 1;
+	mpz_t a, zero, nmoda, one;
 
-	while (n % a != 0 && a <= n)
-		a++;
-	printf("%lld=%lld*%lld\n", n, n / a, a);
+	mpz_init(a);
+	mpz_init(one);
+	mpz_init(nmoda);
+	mpz_init(zero);
+	mpz_set_str(one, "1", 10);
+	mpz_set_str(zero, "0", 10);
+	mpz_set_str(a, "2", 10);
+	while (flag)
+	{
+		mpz_mod(nmoda, n, a);
+		if (mpz_cmp(nmoda, zero) != 0)
+			mpz_add(a, a, one);
+		else
+		{
+			flag = 0;
+			break;
+		}
+	}
+	mpz_tdiv_q(one, n, a);
+	gmp_printf("%Zd=%Zd*%Zd\n", n, one, a);
+	mpz_clear(nmoda);
+	mpz_clear(a);
+	mpz_clear(one);
+	mpz_clear(zero);
 }
